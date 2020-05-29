11451d6f70ae56fad10d35516ffe012ca9cf3c54
nicksonpoon <thenicksonpoon@gmail.com>
2020-05-29 07:37:34 -0400

    Add Firebase and user authentication

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +16,19 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
-    }
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    //MARK: - Error handling needed - Alert for user perhaps
+                    print(error.localizedDescription)
+                    } else {
+                        // Navigate to the ChatViewController
+                        self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    }
+                }
+            }
     
+    }
 }
+
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,23 +7,28 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        titleLabel.text = ""
-        var charIndex = 0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.15 * Double(charIndex), repeats: false) { (Timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        
+        titleLabel.text = "⚡️FlashChat"
+        
+        
+// Same animation for titleLabel without CLTypingLabel
+//        titleLabel.text = ""
+//        var charIndex = 0
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.15 * Double(charIndex), repeats: false) { (Timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
         
     }
     
