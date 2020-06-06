c637e9cc39dea1e03d193524b915906babdfe359
nicksonpoon <thenicksonpoon@gmail.com>
2020-06-06 16:22:35 -0400

    Setup Firebase database and saving data to firestorm

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,6 +14,8 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
     var messages: [Message] = [
         Message(sender: "1@2.com", body: "Hey"),
         Message(sender: "2@1.com", body: "Hello"),
@@ -31,6 +33,21 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody])
+            { (error) in
+                if let error = error {
+                    print("Error\(error)")
+                } else {
+                    print("Saved data")
+                }
+            }
+            
+        }
+        
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
