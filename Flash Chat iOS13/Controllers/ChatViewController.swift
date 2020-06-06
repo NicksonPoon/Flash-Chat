0572cf46abe14bb378b29c0c78f566db0e65f38f
nicksonpoon <thenicksonpoon@gmail.com>
2020-06-06 15:10:17 -0400

    Create custom MessageCell xib

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -26,6 +26,8 @@ class ChatViewController: UIViewController {
         tableView.dataSource = self
         navigationItem.title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -49,8 +51,9 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+        cell.label.text = messages[indexPath.row].body
         return cell
     }
     
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Nickson Poon on 6/6/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet var messageBubble: UIView!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        // Corner Radius adapts to the height of the message
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 3
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
